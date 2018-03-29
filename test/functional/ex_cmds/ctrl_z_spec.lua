8c75bd0ecff29755be96a86da62ec7bf56ebe9ef
geekodour <hrishikeshbman@gmail.com>
2018-03-29 13:19:06 +0530

    Added unuseful test

--- test/functional/ex_cmds/ctrl_z_spec.lua
+++ test/functional/ex_cmds/ctrl_z_spec.lua
@@ -1,13 +1,17 @@
 local helpers = require('test.functional.helpers')(after_each)
 local Screen = require('test.functional.ui.screen')
-local clear, feed, source = helpers.clear, helpers.feed, helpers.source
+local clear, feed, spawn, set_session, eq = helpers.clear, helpers.feed, helpers.spawn, helpers.set_session, helpers.eq
 local command = helpers.command
 
 
 describe("CTRL-Z Actions", function()
   local screen
+  local nvim_argv = {helpers.nvim_prog, '-u', 'NONE', '-i', 'NONE', '-N', '--embed'}
 
   before_each(function()
+    clear()
+    local screen_nvim = spawn(nvim_argv)
+    set_session(screen_nvim)
     screen = Screen.new(52, 6)
     screen:attach()
   end)
@@ -18,6 +22,8 @@ describe("CTRL-Z Actions", function()
         eq(true, screen.suspended)
       end
       feed('<c-z>')
+      -- custom feed
+      -- if c-z is recieved, it should directly call SIGSTOP
       screen:expect(check)
   end)
 
